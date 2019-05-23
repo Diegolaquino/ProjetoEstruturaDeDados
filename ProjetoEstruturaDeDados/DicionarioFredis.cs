b8e12b4d452322e5c105647827bd243f6b0d973f
Diego Aquino <diegol.aquino@gmail.com>
2019-05-22 21:53:36 -0300

    Add project files.

---
+++ ProjetoEstruturaDeDados/DicionarioFredis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoEstruturaDeDados
+{
+    class DicionarioFredis
+    {
+        public DicionarioFredis(string chave, int valor)
+        {
+            Chave = chave;
+            Valor = valor;
+        }
+        public string Chave { get; set; }
+        public int Valor { get; set; }
+    }
+}
---
+++ ProjetoEstruturaDeDados/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoEstruturaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool transacao = false;
+            //Queue -> fila
+            var dicionarioFredis = new Dictionary<string, string>();
+
+            //Stack<String> teste = new Stack<string>();
+            //teste.
+
+            //Stack -> pilha
+            while (true)
+            {
+                Console.Write("> ");
+                var entrada = Console.ReadLine().Split(" ");
+
+                switch (entrada[0].ToLower())
+                {
+                    case "set":
+                        if (dicionarioFredis.ContainsKey(entrada[1]))
+                        {
+                            dicionarioFredis[entrada[1]] = entrada[2];
+                        }
+                        else
+                        {
+                            dicionarioFredis.Add(entrada[1], entrada[2]);
+                        }
+
+                        Console.WriteLine("Ok!");
+
+                        break;
+                    case "get":
+                        if(dicionarioFredis.ContainsKey(entrada[1]))
+                        {
+                            Console.WriteLine(dicionarioFredis[entrada[1]]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("(nil)");
+                        }
+                        break;
+                    case "del":
+                        dicionarioFredis.Remove(entrada[1]);
+                        Console.WriteLine("Ok!");
+                        break;
+                    case "add":
+                        dicionarioFredis.Add(entrada[1].ToLower(), entrada[2].ToLower());
+                        break;
+                    case "count":
+                        Console.WriteLine(dicionarioFredis.Count);
+                        break;
+                    case "begin":
+                        transacao = true;
+                        break;
+                    default:
+                        Console.WriteLine("Comando inválido");
+                        break;
+                }
+            }
+
+        }
+
+        
+    }
+}
